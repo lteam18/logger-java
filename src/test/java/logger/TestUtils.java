9f2c08077ad1d0dc9023d0c0d6bc9fae25148b4b
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-13 23:54:00 +0800

    About #11

--- src/test/java/logger/TestUtils.java
+++ src/test/java/logger/TestUtils.java
@@ -1,9 +1,20 @@
-import logger.Utils;
+package logger;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestUtils {
     @Test
     public void testSleep() {
+        System.out.println("123");
         Utils.sleep(3);
+
+        final long t = System.currentTimeMillis();
+        Utils.sleep(3000);
+        final long elapsed = System.currentTimeMillis() - t;
+
+        Assert.assertEquals("Nearly 3 second", elapsed, 3000, 100);
+
+
     }
 }
